b54f4c26a7fc33f59dc439523b5855afb05c5fd1
Weidsom Nascimento <source@snakesecurity.org>
2025-05-09 11:53:08 -0700

    Add AFL++ source code 4.32

---
+++ AFOSBUILD.sh
@@ -0,0 +1,49 @@
+if [ $(uname -m | grep 'x86_64') ]; then
+   make distrib
+
+   if [ $? -eq 0 ]
+   then
+     # Result is OK! Just continue...
+     echo "Make all... PASS!"
+   else
+     # houston we have a problem
+     exit 1
+   fi
+
+   make man
+   make install
+
+   if [ $? -eq 0 ]
+   then
+     # Result is OK! Just continue...
+     echo "Make install... PASS!"
+   else
+     # houston we have a problem
+     exit 1
+   fi
+
+else
+   make AFL_NO_X86=1 distrib
+
+   if [ $? -eq 0 ]
+   then
+     # Result is OK! Just continue...
+     echo "Make all... PASS!"
+   else
+     # houston we have a problem
+     exit 1
+   fi
+
+   make man
+   make install
+
+   if [ $? -eq 0 ]
+   then
+     # Result is OK! Just continue...
+     echo "Make install... PASS!"
+   else
+     # houston we have a problem
+     exit 1
+   fi
+
+fi
